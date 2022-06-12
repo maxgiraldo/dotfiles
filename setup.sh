9e03b86caff09679b2c728f96facc4aa64df0549
Maximilian A. Giraldo <maxgiraldo@hey.com>
2022-06-12 15:33:13 -0400

    stop using ansible, use bash script instead

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 xcode-select --install
 sudo xcodebuild -license
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-pip install --ignore-installed ansible
-ansible-galaxy install -r requirements.yml
 
-#if you need sudo --ask-become-pass
-ansible-playbook -i "localhost," -c local mac.yml
+echo "Installing homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing packages..."
+brew bundle install
+echo "Install oh my zsh..."
+sh -c '$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
+echo "Setting ZSH as default shell..."
+chsh -s "$(which zsh)"
+echo "Installing zsh autosuggestions plugin..."
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
+echo "Updating zshrc..."
+sed -i '' 's/(git/(git zsh-autosuggestions/g' ~/.zshrc
